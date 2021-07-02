f974a96767f7ac12ba73165072e8de2a0e685999
ahcantarim <andre.cantarim@gmail.com>
2021-07-02 20:39:31 -0300

    Tutorial 1: Producer e Consumer

---
+++ src/Tutorial.RabbitMQ/Tutorial.RabbitMQ.Console.Receive/Receive.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Tutorial.RabbitMQ.Console.Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var queueName = "hello";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+
+                channel.BasicConsume(queueName, true, consumer);
+
+                System.Console.WriteLine(" Press [enter] to exit.");
+                System.Console.ReadLine();
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            System.Console.WriteLine($" [x] Received '{message}'");
+        }
+    }
+}
---
+++ src/Tutorial.RabbitMQ/Tutorial.RabbitMQ.Console.Send/Send.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Tutorial.RabbitMQ.Console.Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var queueName = "hello";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+
+                string message = $"Hello World at {DateTime.Now}!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, queueName, null, body);
+
+                System.Console.WriteLine($" [x] Sent '{message}'");
+            }
+
+            System.Console.WriteLine($" Press [enter] to exit.");
+            System.Console.ReadLine();
+        }
+    }
+}
